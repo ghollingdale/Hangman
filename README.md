0d79e3e95901bc44363129ac2a1fde732c3c62be
George Hollingdale <g.hollingdale@outlook.com>
2023-06-15 14:10:40 +0200

    Prorgram works. README.md is finished. This is the fina version.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Hangman
-My hangman OOP project
+#### *A George Hollingdale game written in Python 3.11*
+***
+This is a basic Hangman game written in Python. The goal of the game is to guess a secret word, guessing letter by letter. If all of the letters of the word have been guessed before 5 incorrect attempts, then the player wins.
+
+# Installation
+You will need to have Python installed to your device to run this program. It is executable in your devices terminal.
+# Usage
+To play the game:   
+* Run the program
+* Input a letter into the field and hit enter
+    * If the input is a valid (lowercase letter, not already chosen) it will be accepted
+    * If the input is invalid (e.g. a number, more than one leter etc.) it will be rejected and the user is prompted to enter a new letter
+* The program checks the letter against the secret word
+    * If the letter **is** in the secret word, the letter is shown and the user moves on to a new guess
+    * If the letter **isn't** in the secret word, the user loses a life and is prompted to give a new guess
+* The game continues to cycle until either five lives are lost, or the word is correctly guessed!
+ To play the game again, kill the terminal and reboot the program.
+
+# Timeline
+The game took me around 8-9 hours to complete.
+* The main code took me about 6-7 hours including testing/debugging
+* The program aesthetics including print text and output formatting took around 1-2 hourIs
+
+# Personal situation
+I am a junior at Becode following an AI bootcamp. I had  zero programming experience before May '23. This is my first full project in Python. 
--- utils/game.py
+++ utils/game.py
@@ -1,11 +1,11 @@
-#importing random module
+# Importing random module
 import random
 #Defining Hangman class
 class Hangman:
     pass
-    #Initializing attributes
+    
+    #Sets attributes needed for program.
     def __init__(self):
-        #set attritubres
         self.possible_words = ['becode', 'learning', 'mathematics', 'sessions']
         self.word_to_find = list(random.choice(self.possible_words))
         self.lives = 5
@@ -15,7 +15,8 @@ class Hangman:
         self.wrongly_guessed_letters = []
         self.turn_count = 0
 
-    #Start the game loop and check whether win/loss conditions are met. Load respective method.
+    # Start the game loop and check whether win/loss conditions are met. 
+    # Loads respective method.
     def game_start(self):
         print("You have" , self.lives , "live(s) remaining.")
         print("Current progress:" , *self.correctly_guessed_letters)
@@ -28,11 +29,12 @@ class Hangman:
         else:
             self.You_lose()
 
-    #Manage a game turn and update the turn/board status 
-    ###INCOMPLETE - need to work on enumeration and indexing
+    # Manages a game turn. 
+    # Calls checkvalidity and prints Error if not met, then restarts method. 
+    # Changes uppercase inputs to lower.
+    # Checks whether guess is in word to find and moves the game forward.
     def play(self):
         guess = input("\nPlease enter a letter as your guess: ")
-        #checks validity of input. Sends user back to input if invalid
         while self.checkvalidity(guess) == False:
             print("\nError. Please enter a single lowercase letter that hasn't been guessed yet.")
             print("You have" , self.lives , "live(s) remaining.")
@@ -40,7 +42,6 @@ class Hangman:
             print("You have already used these letters:" , *self.wrongly_guessed_letters)
             self.play()
         guess = guess.lower()
-        #checks whether the guess is in the secret word. Increases turn counter. Replaces dash in correctly_guessed_letters with the guess, in the correct position.
         if guess in self.word_to_find:
             print("\nCorrect!", guess , "is in the secret word!")
             self.turn_count += 1
@@ -48,39 +49,32 @@ class Hangman:
                 if guess == correct_letter:
                     self.correctly_guessed_letters[position] = guess
             self.game_start()
-        #checks whether guess isn't in word_to_find, then increases turn_counter and incorrect_guess_count, and reduces lives
-        #Appends the incorrect guess to wrongly_guessed_letters
         elif guess not in self.word_to_find:
             self.turn_count += 1
-            #self.incorrect_guess_count += 1
             self.lives -= 1
             self.wrongly_guessed_letters.append(guess)
             print("\nIncorrect guess.")
             self.game_start()
         else:
-            pass
-         
+            pass                                  
 
-
-                                    
-
-    ##Win function
+    # Win function.
+    # Resets the game and prints congratulatory message.
+    # Quits program
     def You_win(self):
-        self.correctly_guessed_letters = []
-        self.wrongly_guessed_letters = []
         print("\nCongratulations you have won with" , self.lives , "live(s) remaining!")
         return quit()
-        
     
-    ##Lose function
+    # Lose function
+    # Resets the game and prints loser message.
+    # Quits program
     def You_lose(self):
-        self.correctly_guessed_letters = []
-        self.wrongly_guessed_letters = []
         print("\nOut of lives! You lose. Too bad. Better luck next time.")
         return quit()
 
-    ##An attempt at checking validity. 
-    ## Not yet tested.
+    # Validity check.
+    # Returns False if input is longer than one character, not in the alphabet, or already in wrongly_guessed_letters or correctly_guessed_letters.
+    # Otherwise accepts input and returns True.
     def checkvalidity(self , guess):
         if len(guess) != 1:
             return False
@@ -92,4 +86,3 @@ class Hangman:
             return False
         else:
             return True
-
